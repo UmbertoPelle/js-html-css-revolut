102092765f625a21c4a6bcd375a9429d896a7cf4
Umberto <umbertopellegrino96@gmail.com>
2020-07-06 17:46:16 +0200

    hamburger menu

--- js/script.js
+++ js/script.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
   $("#top-nav .drop-down").mouseleave(function(){
     $(".drop-down-content").hide();
   });
+
+  $(".hamburger-dropdown-parent").click(function(){
+    $(".hamburger-dropdown").show();
+  });
+
+  $(".hamburger-dropdown .item h3").click(function(){
+    $(".hamburger-dropdown .item ul").slideUp()
+    $(this).next().slideToggle();
+  });
+
+  $(".hamburger-dropdown .close").click(function(){
+    $(".hamburger-dropdown").slideUp();
+  });
 });
